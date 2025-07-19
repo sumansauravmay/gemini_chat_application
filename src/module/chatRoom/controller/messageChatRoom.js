aedc617da800a3e361a87779d19d40024cc2c108
sumansauravmay <sumansauravmay12345@gmail.com>
2025-07-19 16:06:05 +0530

    integrated gemini api and built message api

---
+++ src/module/chatRoom/controller/messageChatRoom.js
@@ -0,0 +1,52 @@
+const { pool } = require("../../../config/db");
+const { generateGeminiContent } = require("../../../services/geminiApi");
+const {
+  insertMessageQuery,
+  createMessagesTable,
+} = require("../chatRoom.queries");
+
+// Create table (ensure it exists)
+const initializeMessagesTable = async () => {
+  try {
+    await pool.query(createMessagesTable);
+    console.log("Messages table is ready");
+  } catch (error) {
+    console.error("Error creating users table:", error);
+  }
+};
+
+const messageChatRoom = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const chatroomId = req.params.id;
+    const { message } = req.body;
+
+    if (!message) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Message required" });
+    }
+    await pool.query(insertMessageQuery, [chatroomId, "user", userId, message]);
+
+    const geminiResponse = await generateGeminiContent(message);
+    await pool.query(insertMessageQuery, [
+      chatroomId,
+      "gemini",
+      null,
+      geminiResponse,
+    ]);
+
+    return res.status(200).json({
+      success: true,
+      userMessage: message,
+      geminiResponse,
+    });
+  } catch (error) {
+    console.error("Error in messageChatRoom:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+module.exports = { messageChatRoom, initializeMessagesTable };
