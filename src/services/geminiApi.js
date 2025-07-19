9256d9c0d627e6aec3c60472ccb7e4181c6642ca
sumansauravmay <sumansauravmay12345@gmail.com>
2025-07-19 16:06:47 +0530

    built message api

--- src/services/geminiApi.js
+++ src/services/geminiApi.js
@@ -1,22 +1,33 @@
 // services/geminiService.js
 const axios = require("axios");
 
-const GEMINI_API_KEY = process.env.gemini_API_KEY;
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require("dotenv").config();
 
-const generateGeminiContent = async (data) => {
+const GEMINI_API_KEY =
+  "AIzaSyC_H1z7YzxzI1A9_CW518Y_qzGebFYNNUY" || process.env.GEMINI_API_KEY;
+
+if (!GEMINI_API_KEY) {
+  console.error("Error: GEMINI_API_KEY not found in .env file. Please set it.");
+  process.exit(1);
+}
+
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const generateGeminiContent = async (userMessage) => {
+  const prompt = userMessage;
+
   try {
-    const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${GEMINI_API_KEY}`,
-      data
-    );
-
-    const textResponse =
-      response.data?.candidates?.[0]?.content?.parts?.[0]?.text || "";
-    return textResponse;
+    const result = await model.generateContent(prompt);
+    const response = result.response.text();
+    // console.log("Gemini Reply:", response);
+    return response;
   } catch (error) {
-    console.error("Gemini API error:", error?.response?.data || error.message);
-    throw new Error("Failed to generate content using Gemini API");
+    // console.error("Error generating Gemini reply:", error);
+    return "Sorry, something went wrong with Gemini.";
   }
 };
+
 
 module.exports = { generateGeminiContent };
