6ab9c5247c181ec2de5a9922cabf656a77cf08c8
sumansauravmay <sumansauravmay12345@gmail.com>
2025-07-19 12:51:26 +0530

    get all chat rooms

--- src/module/chatRoom/chatRoom.route.js
+++ src/module/chatRoom/chatRoom.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { authMiddleware } = require("../../middlewares/authMiddleware");
-const { createChatRoom } = require("./controller/createChatRoom");
+const { createChatRoom, getAllChatRooms } = require("./controller/createChatRoom");
 
 const chatrouter = express.Router();
 
 chatrouter.post("/chatroom", authMiddleware, createChatRoom);
+chatrouter.get("/chatroom", authMiddleware, getAllChatRooms);
 
 module.exports = { chatrouter };
